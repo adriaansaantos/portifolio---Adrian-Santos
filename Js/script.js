64e8b7860ed7296b46f4dc5f0cacfd2ca69f7349
adriaansaantos <adriandossantos76@gmai.com>
2024-03-13 22:19:58 -0300

    Commit (13/03)

--- Js/script.js
+++ Js/script.js
@@ -9,11 +9,7 @@ document.addEventListener('scroll', function() {
 
     let value = window.scrollY
 
-    
     title.style.marginTop = value * 1.1 + 'px'
-
-    leaf1.style.marginLeft = -value + 'px'
-    leaf2.style.marginLeft = value + 'px'
 
     bush2.style.marginBottom = -value + 'px'
 
